28326366c40528b201f8a58a5f1df188241dc242
林倚農 <jakeusgay@linyinongdeMBP.lan>
2024-11-16 16:13:25 -0500

    Using Wav2Vec2 model to replace WavLM model, adding compute mean and variance function

---
+++ src/model/C_DSVAE.py
@@ -0,0 +1,44 @@
+# Load model directly
+from transformers import Wav2Vec2Config, Wav2Vec2Model, Wav2Vec2Processor
+from sklearn.cluster import KMeans
+import numpy as np
+import torch
+
+class Wav2Vec2:
+
+    def __init__(self, model_name = 'facebook/wav2vec2-base-960h', n_clusters = 50):
+        self.processor = Wav2Vec2Processor.from_pretrained(model_name)
+        self.model = Wav2Vec2Model.from_pretrained(model_name)
+        self.n_clusters = n_clusters
+        self.kmeans = KMeans(n_clusters = n_clusters, random_state = 0)
+
+    def extract_wav2vec2_features(self, audio_data):
+        inputs = self.processor(audio_data, sampling_rate = 16000, return_tensors = "pt", padding=True)
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+        return outputs.last_hidden_state  
+
+    def generate_pseudo_labels(self, features):
+        labels = self.kmeans.fit_predict(features)
+        return labels
+    
+    def compute_cluster_mean_and_variance(self, features, labels):
+        cluster_means = []
+        cluster_variances = []
+
+        for cluster_id in range(self.n_clusters):
+            cluster_features = features[labels == cluster_id]
+
+            cluster_mean = np.mean(cluster_features, axis = 0)
+            cluster_variance = np.var(cluster_features, axis = 0)  
+
+            cluster_means.append(cluster_mean)
+            cluster_variances.append(cluster_variance)
+        
+        return np.array(cluster_means), np.array(cluster_variances)
+
+
+
+
+        
+    
