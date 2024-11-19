530543d70eaaa4e664984a1a87a03d7e15de97dc
林倚農 <jakeusgay@linyinongdeMBP.lan>
2024-11-19 16:55:18 -0500

    Added studentteacher folder and train.py under it, move two mel functions into this train.py

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 import librosa
 import numpy as np
 import torch
-from sklearn.metrics.pairwise import cosine_similarity
 from torch import nn
 
 
@@ -14,35 +13,7 @@ def reparameterize(mean, logvar):
 
     return mean + epsilon * std
 
-def melspectrogram_cosine_similarity(mel1, mel2):
-    mel_1 = np.array(mel1)
-    mel_2 = np.array(mel2)
 
-    log_mel1 = librosa.power_to_db(mel_1, ref=np.max)
-    log_mel2 = librosa.power_to_db(mel_2, ref=np.max)
-
-    vector1 = log_mel1.flatten().reshape(1,-1)
-    vector2 = log_mel2.flatten().reshape(1,-1)
-
-    similarity = cosine_similarity(vector1, vector2)
-
-    return similarity
-
-
-def melspectrogram_mse(mel1, mel2):
-    mel_1 = np.array(mel1)
-    mel_2 = np.array(mel2)
-
-    log_mel1 = librosa.power_to_db(mel_1, ref=np.max)
-    log_mel2 = librosa.power_to_db(mel_2, ref=np.max)
-
-    min_shape = (min(log_mel1.shape[0], log_mel2.shape[0]), min(log_mel1.shape[1], log_mel2.shape[1]))
-    log_mel1 = log_mel1[:min_shape[0], :min_shape[1]]
-    log_mel2 = log_mel2[:min_shape[0], :min_shape[1]]
-
-    mse = np.mean((log_mel1 - log_mel2) ** 2)
-
-    return mse
 
 
 def kld_with_any(mean_c, logvar_c): ...
