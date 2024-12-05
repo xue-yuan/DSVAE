dae80c6bb5e0a93302648726a80cafae574a6d0a
Ares <denon@xueyuan.dev>
2024-12-05 17:32:48 -0500

    Update training process

--- train.py
+++ train.py
@@ -1,13 +1,15 @@
 import torch
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, RandomSampler
 import torch.nn.functional as F
-from torchaudio import datasets, transforms
+from torchaudio import datasets
 
-from dataset import collate_fn_fixed
+from loader import collate_function
 from models import DSVAE
 
-
-resampler = transforms.Resample(orig_freq=48000, new_freq=16000)
+has_mps = torch.backends.mps.is_built()
+device = "mps" if has_mps else "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device {device}")
+torch.mps.empty_cache()
 
 
 def kld_with_any(mean_c, logvar_c, mean_wav, var_wav):
@@ -30,41 +32,40 @@ def loss_function(x, recon_x, mean_s, logvar_s, mean_c, logvar_c):
     return (
         recon_loss(x, recon_x)
         + kld_with_normal(mean_s, logvar_s)
-        + kld_with_any(mean_c, logvar_c)
+        # + kld_with_any(mean_c, logvar_c)
     )
 
 
 train_data = datasets.VCTK_092("./", download=True)
-fixed_length = 794730
 train_data_loader = DataLoader(
     train_data,
-    batch_size=128,
-    collate_fn=lambda batch: collate_fn_fixed(batch, fixed_length=fixed_length),
-    shuffle=True,
+    batch_size=4,
+    collate_fn=lambda batch: collate_function(batch),
+    sampler=RandomSampler(train_data, num_samples=100),
 )
 
-
 model = DSVAE()
+model.to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-epochs = 100
+epochs = 1
 losses = []
 
-
+torch.mps.empty_cache()
 for epoch in range(epochs):
     model.train()
     train_loss = 0
 
-    for x, _ in train_data_loader:
-        x = x.unsqueeze(1)
-        x_resampled = resampler(x)
+    for batch_idx, (x, _) in enumerate(train_data_loader):
+        x = x.to(device)
         optimizer.zero_grad()
 
-        recon_x, mean_s, logvar_s, mean_c, logvar_c = model(x_resampled)
-        loss = loss_function(x_resampled, recon_x, mean_s, logvar_s, mean_c, logvar_c)
+        recon_x, mean_s, logvar_s, mean_c, logvar_c = model(x)
+        loss = loss_function(x, recon_x, mean_s, logvar_s, mean_c, logvar_c)
         loss.backward()
 
         train_loss += loss.item()
         optimizer.step()
+        print(f"Epoch {epoch + 1}, Batch {batch_idx + 1}/{len(train_data_loader)}")
 
     avg = train_loss / len(train_data_loader.dataset)
     losses.append(avg)
